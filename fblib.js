487d130448f93f815b106e884ab9cf88985b197e
cbezault <curtbezault@gmail.com>
2013-09-07 16:30:41 -0400

    Did some work on the fb stuff

--- fblib.js
+++ fblib.js
@@ -1,30 +1,10 @@
 function setup(){
 	FB.getLoginStatus(function(response){
 		if (response.status === 'connected'){
-			var uid = response.authResponse.userID;
 			var accessToken = response.authResponse.accessToken;
-			var a = document.getElementById("uid");
-			a.innerHTML=uid;
-			var b = document.getElementById("accessToken");
-			b.innerHTML=accessToken;
-			}
-		//else if (response.status === 'not_authorized'){
-				//FB.login(function(response){
-					//if(response.authResponse){
-				//FB.ui();
-		//	}
-		else{
-		
+			$.getJSON($SCRIPT_ROOT + "/POST", accessToken);
+		else{	
 		}
 	})
 };
-
-function compare(other, me){
-	var friendsArray;
-	$.getJSON("https://graph.facebook.com/" + me + "/mutualfriends/" + other, function(data) {
-		friendsArray = other;
-	});
-	return friendsArray;
-};
-
 
