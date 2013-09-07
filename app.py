cae3c8231f2828d31053e58f339d48bc57c73d66
Daryl Sew <darylsew@gmail.com>
2013-09-07 15:30:39 -0400

    AJAX WORKS OH MY GOD THIS IS AMAZING. GO JSON. GO CALLBACKS. I LOVE EVERYTHING. I LOVE YOU.

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'GET': 
-        return render_template('gps.html')
-    elif request.method == 'POST':
-        latitude = request.form['latitude']
-        longitude = request.form['longitude']
-        print "latitude %s" % latitude
-        print "longitude %s" % longitude
-        return "whee!"
+    return render_template("gps.html")
+
+@app.route('/location/')
+def handle_location(lat=None, lon=None):
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    return (lat, lon)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
