960a9cb82c4cbbef4149130eb79637356f00b708
cbezault <curtbezault@gmail.com>
2013-09-07 10:42:38 -0400

    getting the fb

---
+++ fblib.js
@@ -0,0 +1,20 @@
+function setup(){
+	FB.getLoginStatus(function(response){
+		if (response.status === 'connected'){
+			var uid = response.authResponse.userID;
+			var accessToken = response.authResponse.accessToken;
+			var a = document.getElementById("uid");
+			a.innerHTML=uid;
+			var b = document.getElementById("accessToken");
+			b.innerHTML=accessToken;
+			}
+		//else if (response.status === 'not_authorized'){
+				//FB.login(function(response){
+					//if(response.authResponse){
+				//FB.ui();
+			}
+		else{
+		
+		}
+	}
+};
