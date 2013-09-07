c7f6c95a622e45a08bcf620859ab23a2a58f3145
Kevin Wang <kevin@kevinwang.com>
2013-09-07 16:35:33 -0400

    Add frontend for setting/getting user FB access tokens.

--- fdc.py
+++ fdc.py
@@ -4,6 +4,10 @@ Frontend for fdc-server backend.
 """
 import requests
 
+# For testing
+import random
+import string
+
 def is_friend_of_friend(id):
     return True
 
@@ -18,4 +22,29 @@ def friends_of_friends_nearby(latitude, longitude):
     friends_of_friends_nearby = [user for user in users_in_radius if is_friend_of_friend(user)]
     return friends_of_friends_nearby
 
-print friends_of_friends_nearby(0.005,0.0)
+def get_fb_access_token(id):
+    r = requests.get('http://ec2-23-20-163-191.compute-1.amazonaws.com/user/%d/fb_access_token' % id).json()
+
+    if not r['success']:
+        print 'fail'
+        return ''
+
+    token = r['token']
+    return token
+
+def set_fb_access_token(id, token):
+    payload = {'token': token}
+    r = requests.put('http://ec2-23-20-163-191.compute-1.amazonaws.com/user/%d/fb_access_token' % id, params=payload).json()
+
+    if not r['success']:
+        print 'fail'
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    print friends_of_friends_nearby(0.005,0.0)
+    test_token = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+    set_fb_access_token(123, test_token)
+    print 'Test token = ' + test_token
+    print 'GET value = ' + get_fb_access_token(123)
