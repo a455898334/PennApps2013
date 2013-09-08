0bd68788b7089f68f761581aae17530634f61eb3
cbezault <curtbezault@gmail.com>
2013-09-07 20:27:58 -0400

    hiii

--- fblib.js
+++ fblib.js
@@ -1,8 +1,8 @@
 function setup(){
-	FB.getLoginStatus(function(response){
-		if (response.status === 'connected'){
-			var accessToken = response.authResponse.accessToken;
-			$.getJSON($SCRIPT_ROOT + "/POST", accessToken);
+    FB.getLoginStatus(function(response){
+	if (response.status === 'connected'){
+            var accessToken = response.authResponse.accessToken;
+            $.getJSON($SCRIPT_ROOT + "/POST", accessToken);
 		if else(response.status === "not_authorized"){
 		}
 		else{	
