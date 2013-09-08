be688095b3f319d88689228a35057eaa342fb72e
cbezault <curtbezault@gmail.com>
2013-09-08 02:45:51 -0400

    to tired tor remember

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for, escape, request, session
-import fdc, fb, foursquare, misc
+import fdc, fb, foursquare, misc, simplejson, urllib2
 
 app = Flask(__name__)
 app.secret_key = ']\x8d3\t\x0bD\xf9\xc5\x13\xb60\xebGdt$\xc0\x1eo\x12\xf1G\xd2\xa9'
+
+secretKey = "f8d55cc687c3d83b5fca1aaee2f536b8"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,6 +14,11 @@ def index():
     else: 
         # Connect to facebook
         return render_template('home.html') 
+
+@app.route('/loginhandler')
+def login_handler(uid=None, accessToken=None):
+    uri = urllib2.Request("GET /oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret={secretKey}&fb_exchange_token={accessToken}")
+    set_fb_access_token(uid, uri)
 
 @app.route('/logout')
 def logout():
