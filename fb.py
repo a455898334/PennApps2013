2c10e88ef44ba0fefa7c88fddd32ccb656270eb4
Kevin Wang <kevin@kevinwang.com>
2013-09-08 09:13:17 -0400

    count

--- fb.py
+++ fb.py
@@ -1,4 +1,5 @@
 import simplejson, urllib2, requests
+import fdc
 
 def find_mutual(uid1, uid2):
     uri = urllib2.Request("https://graph.facebook.com/" + uid1 + "/mutualfriends/" + uid2 + "?access_token="+get_fb_access_token(uid))
@@ -13,10 +14,9 @@ def find_mutual(uid1, uid2):
 #    return f.data.mutual_friend_count
 
 def num_mutual(uid1, uid2):
-    uri = urllib2.Request("https://graph.facebook.com/" + uid1 + "/mutualfriends/" + uid2 + "?access_token="+get_fb_access_token(uid))
-    f = opener.open(uri)
+    r = requests.get("https://graph.facebook.com/" + uid1 + "/mutualfriends/" + uid2 + "?access_token="+fdc.get_fb_access_token(uid))
     simplejson.load(f)
-    return f.data.length
+    return len(f['data'])
 
 def get_name(id):
     r = requests.get('https://graph.facebook.com/%d' % id).json()
